09a529a24e061b3c6d3d9bb1c9e9ecee990a8212
HoneyAle <johnree2023@gmail.com>
2025-06-14 13:20:48 -0700

    Keyboard Input

--- Copies/Shooter/main.py
+++ Copies/Shooter/main.py
@@ -17,18 +17,29 @@ clock = pygame.time.Clock()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Shooter")
 
+# Define Colors
+BG = (144,201, 120)
+
 # Game Variables
+
+# Define Player Action Variables
+moving_left = False
+moving_right = False
 
 # -----------------------
 # Helper Functions
 # -----------------------
+
+def draw_bg():
+    screen.fill(BG)
 
 
 # -----------------------
 # Game Objects
 # -----------------------
 
-player = Soldier(200, 200, 3)
+player = Soldier('player', 200, 200, 3, 5)
+enemy = Soldier('enemy', 400, 200, 3, 5)
 
 # -----------------------
 # Game Loop
@@ -38,11 +49,29 @@ while run:
 
     clock.tick(FPS)
 
+    draw_bg()
+
     player.draw(screen)
+    enemy.draw(screen)
+    player.move(moving_left, moving_right)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        # keyboard presses
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                moving_left = True
+            if event.key == pygame.K_d:
+                moving_right = True
+        # keyboard released
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a:
+                moving_left = False
+            if event.key == pygame.K_d:
+                moving_right = False
+            if event.key == pygame.K_ESCAPE:
+                run = False
 
 
     pygame.display.update()
--- Copies/Shooter/player.py
+++ Copies/Shooter/player.py
@@ -1,12 +1,34 @@
 import pygame
 
 class Soldier(pygame.sprite.Sprite):
-    def __init__(self, x, y, scale):
+    def __init__(self, char_type, x, y, scale, speed):
         pygame.sprite.Sprite.__init__(self)
-        img = pygame.image.load('img/player/Idle/0.png')
+        self.char_type = char_type
+        self.speed = speed
+        self.direction = 1  # 1=right -1=left
+        self.flip = False
+        img = pygame.image.load(f'img/{self.char_type}/Idle/0.png')
         self.image = pygame.transform.scale(img,(int(img.get_width() * scale), int(img.get_height() * scale)))
         self.rect = self.image.get_rect()
         self.rect.center = (x,y)
 
+    def move(self, moving_left, moving_right):
+        # reset movement variables
+        dx = 0
+        dy = 0
+        # assign movement variables
+        if moving_left:
+            dx = -self.speed
+            self.flip = True
+            self.direction = -1
+        if moving_right:
+            dx = self.speed
+            self.flip = False
+            self.direction = 1
+        # update rect position
+        self.rect.x += dx
+        self.rect.y += dy
+
+
     def draw(self, screen):
-        screen.blit(self.image, self.rect)
+        screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
